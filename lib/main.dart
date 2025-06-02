b949b5eb3e5244620bf705ccea52cee6d07390bf
yooilsong <ilsong963@naver.com>
2025-06-02 12:24:00 +0900

    feat: google sign in setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:cooki/firebase_options.dart';
 import 'package:cooki/presentation/pages/app_entry/app_entry_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -13,12 +15,12 @@ void main() async {
   runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
-      // try {
-      //   await Firebase.initializeApp(
-      //     options: DefaultFirebaseOptions.currentPlatform,
-      //   );
-      // } catch (_) {} // Firebase가 이미 초기화된 경우 무시
-      await dotenv.load(fileName: '.env');
+      try {
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
+      } catch (_) {} // Firebase가 이미 초기화된 경우 무시
+      // await dotenv.load(fileName: '.env');
       // 플러터 프레임워크 내부에서 발생하는 에러
       FlutterError.onError = (errorDetails) {
         FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
