6fdf94179a191b8da994c95e5ec9b49ff5bedd6c
daehan-lim <penjan.eng@gmail.com>
2025-06-08 15:48:30 +0900

    feat: Change app colors to use the primary green color and remove old blue colors

--- lib/app/constants/app_colors.dart
+++ lib/app/constants/app_colors.dart
@@ -5,12 +5,13 @@ abstract class AppColors {
   static const chipGrey = Color(0XFFF7F7F7);
   static const lightGrey = Color(0XFFF1F2F1);
   static const appBarGrey = Color(0xFFF8F9FA);
+  static final widgetBackgroundGreen = Color(0XFFe2f3e9).withValues(alpha: 0.6);
   static final widgetBackgroundBlue = Colors.blue.withValues(alpha: 0.05);
-  static const buttonsBlue = Color(0XFF3C82FF);
+  static final greenTextColor = Color(0XFF3BBC7A);
   static final borderGrey = Colors.grey[400]!;
   static const backgroundGrey = Color(0xFFF2F2F7);
 
-  static const Color inactiveButton = Color(0xFFBDBDBD);
+  static const Color inactiveButton = Color(0xFFEEEEEE);
 
   static const Color kakaoLoginBackgroundColor = Color(0xFFFEE500);
   static const Color googleLoginBackgroundColor = Colors.white;
