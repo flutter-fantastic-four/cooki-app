b328bc868962c77c4018ea3d22fdb0d6fb5b0ccb
daehan-lim <penjan.eng@gmail.com>
2025-06-11 15:57:13 +0900

    feat: Add method to upload image bytes to ImageRepository and ImageStorageDataSource

--- lib/data/repository/image_repository.dart
+++ lib/data/repository/image_repository.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'dart:typed_data';
 import '../data_source/image_storage_data_source.dart';
 
 abstract class ImageRepository {
-  Future<String> uploadImage(File imageFile, String uid, String folder);
+  Future<String> uploadImageFile(File imageFile, String uid, String folder);
+  Future<String> uploadImageBytes(Uint8List imageBytes, String uid, String folder);
 }
 
 class ImageRepositoryImpl implements ImageRepository {
@@ -11,7 +13,12 @@ class ImageRepositoryImpl implements ImageRepository {
   ImageRepositoryImpl(this._dataSource);
 
   @override
-  Future<String> uploadImage(File imageFile, String uid, String folder) {
-    return _dataSource.uploadImage(imageFile, uid, folder);
+  Future<String> uploadImageFile(File imageFile, String uid, String folder) {
+    return _dataSource.uploadImageFile(imageFile, uid, folder);
+  }
+
+  @override
+  Future<String> uploadImageBytes(Uint8List imageBytes, String uid, String folder) {
+    return _dataSource.uploadImageBytes(imageBytes, uid, folder);
   }
 }
