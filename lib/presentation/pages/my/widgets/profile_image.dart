efbc957bc43738de437d573709b953f4e039e729
ilsong963 <ilsong963@naver.com>
2025-06-16 09:47:28 +0900

    feat: 프로필 edit버튼  추가

--- lib/presentation/pages/my/widgets/profile_image.dart
+++ lib/presentation/pages/my/widgets/profile_image.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:cooki/core/utils/dialogue_util.dart';
 import 'package:cooki/core/utils/logger.dart';
-import 'package:cooki/presentation/widgets/category_selection_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cooki/presentation/user_global_view_model.dart';
@@ -16,51 +15,62 @@ class ProfileImage extends ConsumerWidget {
     final user = ref.watch(userGlobalViewModelProvider);
     final vm = ref.read(userGlobalViewModelProvider.notifier);
 
-    return GestureDetector(
-      onTap: () {
-        DialogueUtil.showImagePickerModal(
-          context,
-          onCamera: () => _pickImage(vm, ImageSource.camera),
-          onGallery: () => _pickImage(vm, ImageSource.gallery),
-        );
-      },
-      child: SizedBox(
-        width: 120,
-        height: 120,
-        child: ClipOval(
-          child:
-              user?.profileImage != null && user!.profileImage!.isNotEmpty
-                  ? Image.network(
-                    user.profileImage!,
-                    width: 120,
-                    height: 120,
-                    fit: BoxFit.cover,
-                    loadingBuilder: (context, child, loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return Container(
-                        width: 120,
-                        height: 120,
-                        decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                        child: const Center(child: CircularProgressIndicator(strokeWidth: 2)),
-                      );
-                    },
-                    errorBuilder: (context, error, stackTrace) {
-                      return Container(
-                        width: 120,
-                        height: 120,
-                        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey[200]),
-                        child: Icon(Icons.person, size: 60, color: Colors.grey[400]),
-                      );
-                    },
-                  )
-                  : Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey[200]),
-                    child: Icon(Icons.person, size: 60, color: Colors.grey[400]),
-                  ),
+    return Stack(
+      alignment: AlignmentDirectional.bottomEnd,
+      children: [
+        SizedBox(
+          width: 120,
+          height: 120,
+          child: ClipOval(
+            child:
+                user?.profileImage != null && user!.profileImage!.isNotEmpty
+                    ? Image.network(
+                      user.profileImage!,
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Container(
+                          width: 120,
+                          height: 120,
+                          decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+                          child: const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+                        );
+                      },
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey[200]),
+                          child: Icon(Icons.person, size: 60, color: Colors.grey[400]),
+                        );
+                      },
+                    )
+                    : Container(
+                      width: 120,
+                      height: 120,
+                      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey[200]),
+                      child: Icon(Icons.person, size: 60, color: Colors.grey[400]),
+                    ),
+          ),
         ),
-      ),
+        GestureDetector(
+          onTap: () {
+            DialogueUtil.showImagePickerModal(
+              context,
+              onCamera: () => _pickImage(vm, ImageSource.camera),
+              onGallery: () => _pickImage(vm, ImageSource.gallery),
+            );
+          },
+          child: Container(
+            width: 32,
+            height: 32,
+            decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle, border: Border.all(color: Colors.grey.shade400, width: 2)),
+            child: Icon(Icons.camera_alt, color: Colors.black87, size: 24),
+          ),
+        ),
+      ],
     );
   }
 
