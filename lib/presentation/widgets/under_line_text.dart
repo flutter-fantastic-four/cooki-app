2f8a0016e57886062d207914b13068e90b23d0d8
ilsong963 <ilsong963@naver.com>
2025-06-27 10:29:49 +0900

    feat: 문구 수정

--- lib/presentation/widgets/under_line_text.dart
+++ lib/presentation/widgets/under_line_text.dart
@@ -8,18 +8,22 @@ class UnderLineText extends StatelessWidget {
     this.textColor = AppColors.greyScale500,
     this.underLineColor = AppColors.greyScale500,
     this.textSize = 20.0,
+    this.width = 0.5,
+    this.fontWeight = FontWeight.normal,
   });
 
   final String text;
   final Color textColor;
   final double textSize;
   final Color underLineColor;
+  final double width;
+  final FontWeight fontWeight;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: underLineColor, width: 0.5))),
-      child: Text(text, style: TextStyle(color: textColor, fontSize: textSize)),
+      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: underLineColor, width: width))),
+      child: Text(text, style: TextStyle(color: textColor, fontSize: textSize, fontWeight: FontWeight.normal)),
     );
   }
 }
